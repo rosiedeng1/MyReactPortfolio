ad102d4311b908cfeb3c0709d2d0b87e06c9973f
rosiedeng1 <rd25500@gmail.com>
2024-03-04 23:20:30 -0500

    updated nav endpoints in nav.jsx

---
+++ test/src/pages/data.js
@@ -0,0 +1,46 @@
+
+
+// https://github.com/rosiedeng1/ExpressJSNoteTaker.git
+
+export const portfolio = [
+    {
+      title: "Travel Assistance Application",
+      subtitle: "Javascript",
+      image: "./assets/Travel.png",
+      link: "https://rosiedeng1.github.io/TravelAssistanceApplication/",
+      githubrepo: "https://github.com/rosiedeng1/TravelAssistanceApplication.git"
+
+    },
+    {
+      title: "Weather API",
+      subtitle: "Javascript, API",
+      image: "./assets/WeatherAPI.png",
+      link: "https://rosiedeng1.github.io/Weather-Dashboard-Hw-/",
+      githubrepo: "https://github.com/rosiedeng1/Weather-Dashboard-Hw-.git"
+
+    },
+    {
+      title: "MVC Tech Blog",
+      subtitle: "Javascript",
+      image: "./assets/Travel.png",
+      link: "Open in Local Host",
+      githubrepo: "https://github.com/rosiedeng1/MVC-Tech-Blog-Site.git"
+
+    },
+    {
+      title: "Travel Assistance Application",
+      subtitle: "Javascript",
+      image: "./assets/Travel.png",
+      link: "https://rosiedeng1.github.io/TravelAssistanceApplication/",
+      githubrepo: "https://github.com/rosiedeng1/TravelAssistanceApplication.git"
+
+    },
+    {
+      title: "Travel Assistance Application",
+      subtitle: "Javascript",
+      image: "./assets/Travel.png",
+      link: "https://rosiedeng1.github.io/TravelAssistanceApplication/",
+      githubrepo: "https://github.com/rosiedeng1/TravelAssistanceApplication.git"
+
+    },
+  ];
