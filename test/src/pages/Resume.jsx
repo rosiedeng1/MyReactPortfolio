c7fdeba63490b0849177d2d3710241676a52dba6
rosiedeng1 <rd25500@gmail.com>
2024-03-06 22:14:54 -0500

    added resume download in jsx

--- test/src/pages/Resume.jsx
+++ test/src/pages/Resume.jsx
@@ -1,15 +1,19 @@
 import Header from "../components/Header.jsx"
 import Nav from "../components/Nav.jsx"
 import Footer from "../components/Footer.jsx"
+import { Link } from 'react-router-dom';
 
-// This is a static page mocking an "About Me" section about myself 
+// This is a static page mocking a "Resume" section about myself 
 export default function Resume() {
     return (
         <>
         <Header></Header>
         <Nav></Nav>
         <p>
-Resume: 
+{/* <a href="Resume.png.pdf" target="_blank" rel="noreferrer"> */}
+{/* Here is my resume :)
+    </a> */}
+  <Link to="/assets/Resume.png.pdf" target="_blank" download>Download</Link>
         </p>
         <Footer></Footer>
 
