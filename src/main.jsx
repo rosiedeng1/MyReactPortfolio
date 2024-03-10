e9b44bd626ff8c02eed8deae7d4d879a71c0d553
rosiedeng1 <rd25500@gmail.com>
2024-03-10 00:24:43 -0500

    fixed client.js bug in main jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client.js';
+import ReactDOM from 'react-dom';
 import App from './App.jsx'
 import './index.css'
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
