44f0492f596747aa0108f196d201eaf96b99e25e
rosiedeng1 <rd25500@gmail.com>
2024-03-11 22:50:20 -0400

    added bootstrap to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,207 +1,37 @@
-const Footer = () => {
-    return (
-      <footer>
-         <li>
-  <a href='https://www.linkedin.com/in/rosie-deng-2b71a9122/' className='footer text-white'>
-  Linkedin Profile
-   </a >
-   </li>, 
-   <li>
-  <a href='https://github.com/rosiedeng1' className='footer text-white'>
-  Github Profile
-   </a>
-   </li>, 
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
-//   import React from 'react';
-//   import {
-//     MDBFooter,
-//     MDBContainer,
-//     MDBIcon,
-//     MDBInput,
-//     MDBCol,
-//     MDBRow,
-//     MDBBtn
-//   } from 'mdb-react-ui-kit';
-  
-//   export default function App() {
-//     return (
-//       <MDBFooter className='text-center' color='white' bgColor='dark'>
-//         <MDBContainer className='p-4'>
-//           <section className='mb-4'>
-//             <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
-//               <MDBIcon fab icon='facebook-f' />
-//             </MDBBtn>
-  
-//             <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
-//               <MDBIcon fab icon='twitter' />
-//             </MDBBtn>
-  
-//             <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
-//               <MDBIcon fab icon='google' />
-//             </MDBBtn>
-  
-//             <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
-//               <MDBIcon fab icon='instagram' />
-//             </MDBBtn>
-  
-//             <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
-//               <MDBIcon fab icon='linkedin-in' />
-//             </MDBBtn>
-  
-//             <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
-//               <MDBIcon fab icon='github' />
-//             </MDBBtn>
-//           </section>
-  
-//           <section className=''>
-//             <form action=''>
-//               <MDBRow className='d-flex justify-content-center'>
-//                 <MDBCol size="auto">
-//                   <p className='pt-2'>
-//                     <strong>Sign up for our newsletter</strong>
-//                   </p>
-//                 </MDBCol>
-  
-//                 <MDBCol md='5' start>
-//                   <MDBInput contrast type='email' label='Email address' className='mb-4' />
-//                 </MDBCol>
-  
-//                 <MDBCol size="auto">
-//                   <MDBBtn outline color='light' type='submit' className='mb-4'>
-//                     Subscribe
-//                   </MDBBtn>
-//                 </MDBCol>
-//               </MDBRow>
-//             </form>
-//           </section>
-  
-//           <section className='mb-4'>
-//             <p>
-//               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum repellat quaerat
-//               voluptatibus placeat nam, commodi optio pariatur est quia magnam eum harum corrupti dicta, aliquam
-//               sequi voluptate quas.
-//             </p>
-//           </section>
-  
-//           <section className=''>
-//             <MDBRow>
-//               <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-//                 <h5 className='text-uppercase'>Links</h5>
-  
-//                 <ul className='list-unstyled mb-0'>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 1
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 2
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 3
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 4
-//                     </a>
-//                   </li>
-//                 </ul>
-//               </MDBCol>
-  
-//               <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-//                 <h5 className='text-uppercase'>Links</h5>
-  
-//                 <ul className='list-unstyled mb-0'>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 1
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 2
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 3
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 4
-//                     </a>
-//                   </li>
-//                 </ul>
-//               </MDBCol>
-  
-//               <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-//                 <h5 className='text-uppercase'>Links</h5>
-  
-//                 <ul className='list-unstyled mb-0'>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 1
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 2
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 3
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 4
-//                     </a>
-//                   </li>
-//                 </ul>
-//               </MDBCol>
-  
-//               <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-//                 <h5 className='text-uppercase'>Links</h5>
-  
-//                 <ul className='list-unstyled mb-0'>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 1
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 2
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 3
-//                     </a>
-//                   </li>
-//                   <li>
-//                     <a href='#!' className='text-white'>
-//                       Link 4
-//                     </a>
-//                   </li>
-//                 </ul>
-//               </MDBCol>
-//             </MDBRow>
-//           </section>
-//         </MDBContainer>
-  
+import { FontAwesomeIcon }  
+    from '@fortawesome/react-fontawesome'; 
+import { faLinkedin, faGithub}  
+    from '@fortawesome/free-brands-svg-icons'; 
+import '../src/index.css'; // Import your custom CSS file
 
-//       </MDBFooter>
-//     );
-//   }
+const Footer = () => { 
+  return ( 
+    <footer className="footer"> 
+      <div className="container"> 
+        <div className="row"> 
+          <div className="col-md-6"> 
+            <p>© 2023 Geek. All rights reserved.</p> 
+          </div> 
+          <div className="col-md-6 text-md-right"> 
+            <ul className="social-icons list-unstyled"> 
+              <li> 
+                <a  
+href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer"> 
+                  <FontAwesomeIcon icon={faLinkedin} /> 
+                </a> 
+              </li> 
+              <li> 
+                <a href= 
+"https://github.com/yourusername" target="_blank" rel="noopener noreferrer"> 
+                  <FontAwesomeIcon icon={faGithub} /> 
+                </a> 
+              </li> 
+            </ul> 
+          </div> 
+        </div> 
+      </div> 
+    </footer> 
+  ); 
+}; 
+  
+export default Footer;
